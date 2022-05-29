b421590e1b84248ddaee3ede95b6c1f44a415616
Anthony Nahoul <anthony.elnahoul@gmail.com>
2022-05-29 20:50:25 +0300

    backend: Adding a function to update user type + fixing it

---
+++ updateUserType.php
@@ -0,0 +1,13 @@
+<?php
+include("connection.php");
+$type = $_POST["type"];
+$id = $_POST["id"];
+$query = $mysqli->prepare("UPDATE users set type= ? WHERE id= ?");
+$query->bind_param("si", $type, $id); 
+$query->execute();
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
+
+?>
