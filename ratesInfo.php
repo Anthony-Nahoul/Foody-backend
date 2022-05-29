903f4fe05fd09f84b77bc136f0d2895e79f999f9
Anthony Nahoul <anthony.elnahoul@gmail.com>
2022-05-30 01:26:35 +0300

    backend: Getting info for each restaurant rates

---
+++ ratesInfo.php
@@ -0,0 +1,15 @@
+<?php
+include("connection.php");
+$restaurantid = $_POST["restaurantid"];
+$query = $mysqli->prepare("SELECT count(id), avg(value) from rates where restaurants_id  = ?");
+$query->bind_param("i", $restaurantid);
+$query->execute();
+$array = $query->get_result();
+$response = [];
+while($restorate = $array->fetch_assoc()){
+    $response[] = $restorate;
+} 
+$json = json_encode($response);
+echo $json;
+
+?>
