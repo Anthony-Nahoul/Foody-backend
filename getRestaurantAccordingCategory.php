d92fbb497f47dcb8b6744ab346e1fabc9de93b66
Anthony Nahoul <anthony.elnahoul@gmail.com>
2022-05-30 01:27:18 +0300

    backend: Filtering restaurants according to their categories

---
+++ getRestaurantAccordingCategory.php
@@ -0,0 +1,15 @@
+<?php
+include("connection.php");
+$id = $_POST["id"];
+$query = $mysqli->prepare("SELECT piclink, name from restaurants where categories_id  = ?");
+$query->bind_param("i", $id);
+$query->execute();
+$array = $query->get_result();
+$response = [];
+while($resto = $array->fetch_assoc()){
+    $response[] = $resto;
+} 
+$json = json_encode($response);
+echo $json;
+
+?>
