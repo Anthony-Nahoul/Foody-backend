28e2245bd12650b07ae28909f29edc8b17e2c63e
Anthony Nahoul <anthony.elnahoul@gmail.com>
2022-05-30 01:29:55 +0300

    backend: Adding rate for each restaurant from the user

---
+++ addrate.php
@@ -0,0 +1,17 @@
+<?php
+include("connection.php");
+$userid = $_POST["userid"];
+$restaurantid = $_POST["restaurantid"];
+$value = $_POST["value"];
+$approved = 0;
+
+$query = $mysqli->prepare("INSERT INTO rates(users_id, restaurants_id , value) VALUES (?, ?, ?)");
+$query->bind_param("iii", $userid, $restaurantid, $value);
+$query->execute();
+
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
+
+?>
