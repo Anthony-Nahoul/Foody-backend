9f1f3baf363c9be79cdf71427c3ed9c2376dcd73
Anthony Nahoul <anthony.elnahoul@gmail.com>
2022-05-29 22:50:58 +0300

    backend: Adding categories

---
+++ getcategories.php
@@ -0,0 +1,13 @@
+<?php
+include("connection.php");
+$query = $mysqli->prepare("SELECT * from categories");
+$query->execute();
+$arraycategories = $query->get_result();
+$response = [];
+while($category = $arraycategories->fetch_assoc()){
+    $response[] = $category;
+} 
+$json = json_encode($response);
+echo $json;
+
+?>
