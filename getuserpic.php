a954c6f11ab790c7f842f271c70341f14ed77dea
Anthony Nahoul <anthony.elnahoul@gmail.com>
2022-05-29 23:10:48 +0300

    backend: Getting user picture

---
+++ getuserpic.php
@@ -0,0 +1,15 @@
+<?php
+include("connection.php");
+$id = $_POST["id"];
+$query = $mysqli->prepare("SELECT user_piclink from users where id = ?");
+$query->bind_param("i", $id);
+$query->execute();
+$arraypics = $query->get_result();
+$response = [];
+while($profilepic = $arraypics->fetch_assoc()){
+    $response[] = $profilepic;
+} 
+$json = json_encode($response);
+echo $json;
+
+?>
