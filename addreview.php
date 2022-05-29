32f4e6e23614277b6e6840053a1408e64047fe7a
Anthony Nahoul <anthony.elnahoul@gmail.com>
2022-05-29 22:13:04 +0300

    backend: Re-adding addreview (deleted by mistake)

---
+++ addreview.php
@@ -0,0 +1,17 @@
+<?php
+include("connection.php");
+$userid = $_POST["userid"];
+$restaurantid = $_POST["restaurantid"];
+$description = $_POST["description"];
+$approved = 0;
+
+$query = $mysqli->prepare("INSERT INTO reviews(users_id, restaurants_id , review_description, approved) VALUES (?, ?, ?, ?)");
+$query->bind_param("iisi", $userid, $restaurantid, $description, $approved);
+$query->execute();
+
+$response = [];
+$response["success"] = true;
+
+echo json_encode($response);
+
+?>
