cfbc12545ce7c2f3b26559ce636eb98ba322e693
Anthony Nahoul <anthony.elnahoul@gmail.com>
2022-05-29 18:43:15 +0300

    backend: fixing signup hashing for pass and query + all API's successfully tested

--- signup.php
+++ signup.php
@@ -1,15 +1,14 @@
 <?php
 
 include("connection.php");
-
 $firstname = $_POST["firstname"];
 $lastname = $_POST["lastname"];
 $email = $_POST["email"];
-$password = $_POST["password"];
+$password = hash("sha256", $_POST["password"]);
 $userpic = $_POST["userpic"];
 $type = $_POST["type"];
 
-$query = $mysqli->prepare("INSERT INTO users(firstname, lastname, email, password, userpic, type) VALUES (?, ?, ?, ?, ?, ?)");
+$query = $mysqli->prepare("INSERT INTO users(fname, lname, email, password, user_piclink, type) VALUES (?, ?, ?, ?, ?, ?)");
 $query->bind_param("ssssss", $firstname, $lastname, $email, $password, $userpic, $type);
 $query->execute();
 
